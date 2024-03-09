bfd357759391a5a1a598720e9e431f6591bf7177
Bindu Shahi Thakuri <60732406+bindushahi@users.noreply.github.com>
2024-03-09 17:34:11 +0545

    navbar fix for desktop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,35 @@
-import React from "react";
-import '../styles/styles.css'; 
+import React, { useState, useEffect } from "react";
+import "../styles/styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [scrolling, setScrolling] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setScrolling(true);
+    } else {
+      setScrolling(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header" id="intro">
+    <header className={`header ${scrolling ? "scrolling" : ""}`} id="intro">
       {/* Navbar */}
-      <nav className="navbar">
+      <nav className={`navbar  ${menuVisible ? "show" : ""}`}>
         <div className="container">
           <div className="logo">
             <a href="/">
@@ -15,11 +39,12 @@ const Header = () => {
               type="button"
               className="navbar-toggler"
               id="navbar-toggler"
+              onClick={toggleMenu}
             >
-              <i className="fas fa-bars"></i>
+              <FontAwesomeIcon icon={faBars} />
             </button>
           </div>
-          <div className="nav-menu">
+          <div className={`nav-menu ${menuVisible ? "show" : ""}`}>
             <ul>
               <li>
                 <a href="index.html" className="nav-link">
