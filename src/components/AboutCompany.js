a2bb89122f355f23dde655c8bb2c6ac15a1a3bc5
Bindu Shahi Thakuri <60732406+bindushahi@users.noreply.github.com>
2024-03-09 20:29:31 +0545

    company name change

--- src/components/AboutCompany.js
+++ src/components/AboutCompany.js
@@ -5,19 +5,19 @@ const AboutCompany = () => {
   return (
     <section className="about-company">
       <div className="container">
-        <h2>About ABC Company</h2>
+        <h2>About CodePulse </h2>
         <p>
-          Welcome to ABC Company – your gateway to innovation in Nepal. We
+          Welcome to CodePulse – your gateway to innovation in Nepal. We
           specialize in delivering cutting-edge solutions, ranging from bespoke
-          software development to strategic IT consulting. At ABC, we believe in
+          software development to strategic IT consulting. At CodePulse, we believe in
           the power of technology to transform businesses. Our commitment goes
           beyond services; it's about forging lasting partnerships. With a focus
           on precision and quality, we bring a wealth of experience to every
           project. Whether you're looking to streamline operations, enhance user
-          experiences, or embark on a digital transformation journey, ABC
+          experiences, or embark on a digital transformation journey, CodePulse
           Company is your dedicated ally. What sets us apart is our agility and
           adaptability. In a fast-paced digital landscape, we stay ahead of the
-          curve, ensuring your business is equipped for success. Join us at ABC
+          curve, ensuring your business is equipped for success. Join us at CodePulse
           Company, where innovation meets excellence.
         </p>
         <div className="company-image">
